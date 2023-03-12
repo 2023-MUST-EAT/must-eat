35c9f2d19d368b477373647abee76a1cd281a809
Sir-Rain <liciaan@kakao.com>
2023-03-13 00:38:54 +0900

    feat: create restaurant schema

--- packages/server/src/domains/restaurant/restaurant.controller.ts
+++ packages/server/src/domains/restaurant/restaurant.controller.ts
@@ -1,26 +1,6 @@
 import { Request, Response } from "express";
 
-const RestaurantRepository: Repository = {
-    restaurants: [],
-    count: 0
-}
-
-type RestaurantData = {
-    id?: number;
-    name: string;
-    address: string;
-    phone: string;
-    category: string;
-    homepage?: string;
-    menus: string;
-    imageUrl: string;
-    kakaoId: number;
-}
-
-type Repository = {
-    restaurants: RestaurantData[];
-    count: number;
-}
+import { RestaurantRepository } from './schema.js';
 
 
 export function getAllRestaurants(req:Request, res:Response){
